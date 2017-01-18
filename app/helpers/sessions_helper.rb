cefaa80c79ff819c30eb07d420839fa57d2af3d3
Brian <lbrian357@gmail.com>
2017-01-18 06:36:50 +0200

    Finish user edit, update, index and destroy actions

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,14 +1,21 @@
 module SessionsHelper
+  #Logs in the given user.
   def log_in(user)
     session[:user_id] = user.id
   end
 
+  # Remembers a user in a persistent session.
   def remember(user)
     user.remember
     cookies.permanent.signed[:user_id] = user.id
     cookies.permanent[:remember_token] = user.remember_token
   end
   
+  # Returns true if the given user is a current user.
+  def current_user?(user)
+    user == current_user
+  end
+
   def current_user
     if (user_id = session[:user_id])
       @current_user ||= User.find_by(id: session[:user_id])
@@ -20,6 +27,8 @@ module SessionsHelper
       end
     end
   end
+
+
 
   def logged_in?
     !current_user.nil?
@@ -35,6 +44,16 @@ module SessionsHelper
     forget(current_user)
     session.delete(:user_id)
     @current_user = nil
+  end
+
+  #Redirects to stored location ( or to the default).
+  def redirect_back_or(default)
+    redirect_to(session[:forwarding_url] || default)
+    session.delete(:forwarding_url)
+  end
+
+  def store_location
+    session[:forwarding_url] = request.original_url if request.get?
   end
 
 end
--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -2,19 +2,31 @@ require 'test_helper'
 
 class SiteLayoutTest < ActionDispatch::IntegrationTest
 
-  test "layout links" do
+  test "layout links not signed in" do
     get root_path
     assert_template 'static_pages/home'
     assert_select "a[href=?]", root_path, count: 2
-    assert_select "a[href=?]", help_path
-    assert_select "a[href=?]", about_path
-    assert_select "a[href=?]", contact_path
+    assert_select "a[href=?]", help_path, count: 1
+    assert_select "a[href=?]", about_path, count: 1
+    assert_select "a[href=?]", contact_path, count: 1
+    assert_select "a[href=?]", login_path, count: 1
     get contact_path
     assert_select "title", full_title("Contact")
     get signup_path
     assert_select "title", full_title("Sign up")
   end
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "layout links after signing in" do
+    @user = users(:michael)
+    log_in_as(@user)
+    get root_path
+    assert_select "a[href=?]", root_path, count: 2
+    assert_select "a[href=?]", help_path, count: 1
+    assert_select "a[href=?]", about_path, count: 1
+    assert_select "a[href=?]", contact_path, count: 1
+    assert_select "a[href=?]", users_path, count: 1
+    assert_select "a[href=?]", user_path(@user), count: 1
+    assert_select "a[href=?]", edit_user_path(@user), count: 1
+    assert_select "a[href=?]", logout_path, count: 1
+  end
 end
